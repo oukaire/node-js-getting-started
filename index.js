7237ea6af91495c0b7f4f6f011d4f2ddfae6bf34
oukaire <onyinyechi.ukaire@gmail.com>
2017-11-05 16:25:55 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser'); 
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+        response.send("Hello world");
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/pikachu', function(request, response) {
+        console.log(request);
+        response.send("You've won the game!");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.post('/sendLocation', function(request, response) {
+        var theLogin = request.body.login;
+        var theLat = request.body.lat;
+        var theLng = request.body.lng;
+        var returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
+        response.send(returnObject);
 });
+
+app.listen(process.env.PORT || 8888);
