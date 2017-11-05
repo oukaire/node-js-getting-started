e6f00f8cce133e355cfc0fe392e2d4514213ce23
oukaire <onyinyechi.ukaire@gmail.com>
2017-11-05 17:02:54 -0500

    cors added

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 var express = require('express');
-var app = express();
+var cors = require('cors');
 var bodyParser = require('body-parser'); 
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
-        response.send("Hello world");
+    response.send("Hello world");
 });
 
 app.get('/pikachu', function(request, response) {
-        console.log(request);
-        response.send("You've won the game!");
+    console.log(request);
+    response.send("You've won the game!");
 });
 
 app.post('/sendLocation', function(request, response) {
-        var theLogin = request.body.login;
-        var theLat = request.body.lat;
-        var theLng = request.body.lng;
-        var returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
-        response.send(returnObject);
+
+    /* enable Cross-Origin Resource Sharing FOR POST `/sendLocation` */
+    app.use(cors());
+
+    var theLogin = request.body.login;
+    var theLat = request.body.lat;
+    var theLng = request.body.lng;
+    var returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
+    response.send(returnObject);
 });
 
 app.listen(process.env.PORT || 8888);
