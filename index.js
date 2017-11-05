b8567d5e69cda68dd169e6eae31b00ab9289a0ee
oukaire <onyinyechi.ukaire@gmail.com>
2017-11-05 17:42:47 -0500

    finished lab7

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 var express = require('express');
-var cors = require('cors');
+var cors = require('cors'); /* to enable CORS */
 var bodyParser = require('body-parser'); 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(request, response) {
-    response.send("Hello world");
-});
-
-app.get('/pikachu', function(request, response) {
-    console.log(request);
-    response.send("You've won the game!");
-});
-
-app.post('/sendLocation', function(request, response) {
-
-    /* enable Cross-Origin Resource Sharing FOR POST `/sendLocation` */
-    app.use(cors());
+app.post('/sendLocation', cors(), function(request, response) {
 
     var theLogin = request.body.login;
     var theLat = request.body.lat;
     var theLng = request.body.lng;
-    //var returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
-    response.send('{"people":[],"landmarks":[]}');
+    var returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
+    // JSON.stringify(returnObject) ? perhaps
+    response.send(returnObject);
 });
 
 app.listen(process.env.PORT || 8888);
