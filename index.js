a9aadc9bbfacddfec1c1c828fefff071dfc7aea3
oukaire <onyinyechi.ukaire@gmail.com>
2017-11-05 21:08:01 -0500

    updated index.js to send appropriate response on POST /sendLocation

--- index.js
+++ index.js
@@ -6,18 +6,14 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(request, response) {
-    response.send('error: not configured for GET commands');
-});
-
 app.post('/sendLocation', cors(), function(request, response) {
 
     var theLogin = request.body.login;
     var theLat = request.body.lat;
     var theLng = request.body.lng;
     var returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
-    // JSON.stringify(returnObject) ? perhaps
-    response.send(returnObject);
+    var returnMessage = JSON.stringify(returnObject);
+    response.send('{"people":[],"landmarks":[]}');
 });
 
 app.listen(process.env.PORT || 8888);
