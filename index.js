d433e2d5b966b08607e69f4ab1992895ad24d63f
oukaire <onyinyechi.ukaire@gmail.com>
2017-11-12 21:34:01 -0500

    created and exported response text

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ var express = require('express');
 var cors = require('cors');                     /* to enable CORS */
 var app = express();
 
+function validReq(login, lat, lng) 
+{
+    if (lat < -90 || lat > 90) return 0;
+    if (lng < -180 || lng > 180) return 0;
+    if (login == '') return 0;
+    return 1;
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
-    response.send('Web server up and running');
+    response.send('Assignment 3 server up and running');
 }); 
 
 app.post('/sendLocation', cors(), function(request, response) {
@@ -15,9 +23,16 @@ app.post('/sendLocation', cors(), function(request, response) {
     var theLogin = request.body.login;
     var theLat = request.body.lat;
     var theLng = request.body.lng;
-    var returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
-    var returnMessage = JSON.stringify(returnObject);
-    response.send('{"people":[],"landmarks":[]}');
+    var resObj = '';
+
+    if (validReq(theLogin, theLat, theLng) == 0) {
+        response.send('{"error":"Whoops, something is wrong with your data!"}');
+    } 
+    else {
+        returnObject = {"login":theLogin,"lat":theLat,"lng":theLng};
+        resObj = {"people":[returnObject],"landmarks":0};
+        response.send(JSON.stringify(resObj));
+    }
 });
 
 app.listen(process.env.PORT || 8888);
